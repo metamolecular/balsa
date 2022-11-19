04de7f00fa5ab1f1e61e496797b9000fbe33bace
Richard Apodaca <rapodaca@metamolecular.com>
2022-11-19 13:33:42 -0800

    Adds read::digit tests. Closes #12.

--- src/read/digit.rs
+++ src/read/digit.rs
@@ -9,3 +9,30 @@ pub fn digit(scanner: &mut Scanner) -> Option<u8> {
         nonzero(scanner)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use super::*;
+
+    #[test]
+    fn not_found() {
+        let mut scanner = Scanner::new("x");
+
+        assert_eq!(digit(&mut scanner), None)
+    }
+
+    #[test]
+    fn zero() {
+        let mut scanner = Scanner::new("0");
+
+        assert_eq!(digit(&mut scanner), Some(0))
+    }
+
+    #[test]
+    fn non_zero() {
+        let mut scanner = Scanner::new("1");
+
+        assert_eq!(digit(&mut scanner), Some(1))
+    }
+}

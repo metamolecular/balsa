6286712daae2df28e5d69727fa93d5356c09bcbc
Richard Apodaca <rapodaca@metamolecular.com>
2022-11-23 15:52:22 -0800

    Adds unit tests for read::atom. Closes #9.

--- src/read/atom.rs
+++ src/read/atom.rs
@@ -17,3 +17,57 @@ pub fn atom(scanner: &mut Scanner) -> Result<Option<AtomKind>, Error> {
         Ok(None)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::feature::{Bracket, Element, Selection, Shortcut, Symbol};
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn unrecognized() {
+        let mut scanner = Scanner::new("X");
+
+        assert_eq!(atom(&mut scanner), Ok(None))
+    }
+
+    #[test]
+    fn star() {
+        let mut scanner = Scanner::new("*");
+
+        assert_eq!(atom(&mut scanner), Ok(Some(AtomKind::Star)))
+    }
+
+    #[test]
+    fn selection() {
+        let mut scanner = Scanner::new("c");
+
+        assert_eq!(
+            atom(&mut scanner),
+            Ok(Some(AtomKind::Selection(Selection::C)))
+        )
+    }
+
+    #[test]
+    fn shortcut() {
+        let mut scanner = Scanner::new("C");
+
+        assert_eq!(
+            atom(&mut scanner),
+            Ok(Some(AtomKind::Shortcut(Shortcut::C)))
+        )
+    }
+
+    #[test]
+    fn bracket() {
+        let mut scanner = Scanner::new("[C]");
+
+        assert_eq!(
+            atom(&mut scanner),
+            Ok(Some(AtomKind::Bracket(Bracket {
+                symbol: Symbol::Element(Element::C),
+                ..Default::default()
+            })))
+        )
+    }
+}
